55b089fe7977856f969bc39f48752acb34a84f22
bill <wtbtucker@gmail.com>
2024-02-20 09:59:24 -0500

    Move segment model into separate file

---
+++ applications/data-collector/src/main/SegmentRecord.py
@@ -0,0 +1,18 @@
+from app import db
+
+'''
+Define the database model
+used to store segments
+'''
+class Segment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    avg_grade = db.Column(db.Float, nullable=False)
+    start_lat = db.Column(db.Float)
+    start_long = db.Column(db.Float)
+    end_lat = db.Column(db.Float)
+    end_long = db.Column(db.Float)
+    distance = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return '<Segment %r' % self.name
--- applications/data-collector/src/main/app.py
+++ applications/data-collector/src/main/app.py
@@ -5,27 +5,18 @@ import sqlite3
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Segments.sqlite3"
 
-'''
-Define the database model
-used to store segments
-'''
 db = SQLAlchemy(app)
-class Segment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    avg_grade = db.Column(db.Float, nullable=False)
-    start_lat = db.Column(db.Float)
-    start_long = db.Column(db.Float)
-    end_lat = db.Column(db.Float)
-    end_long = db.Column(db.Float)
-    distance = db.Column(db.Float, nullable=False)
+from SegmentRecord import Segment
+
+def get_segment() -> Segment:
+    return db.session.execute(db.select(Segment)).one()
 
 @app.route("/")
 def main():
     db.create_all()
     # db.session.add(Segment(id=229781, name="Hawk Hill", avg_grade=5.7, start_lat=37.8331119, start_long=-122.4834356, end_lat=37.8280722, end_long=-122.4981393, distance=2684.8))
     # db.session.commit()
-    segment = db.session.execute(db.select(Segment)).one()
+    segment = get_segment()
     return "Segment: " + segment._asdict()['Segment'].name
 
 
